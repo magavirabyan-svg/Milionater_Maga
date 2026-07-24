01e24c42cfed9fc651ccb1153b92e9ab0f84a668
magavirabyan-svg <margush.virabyan@tumo.org>
2026-07-24 18:16:42 +0400

    js changes

---
+++ js/script.js
@@ -0,0 +1,11 @@
+'use strict'
+let mainGame = document.querySelector('.game-block'),
+    gameWrapper = document.querySelector('.game-wrap'),
+    startBtn = document.querySelector('.stat-btn'),
+    endBtn = document.querySelector('.end-btn'),
+    btnanswers = document.querySelector('.answer')
+blokQuestions = document.querySelector('.question'),
+    helpBtns = document.querySelector('.hints-help'),
+    winBlock = document.querySelector('.wins-block')
+
+
